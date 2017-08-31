15ba08a0cc6ecd295880a305783afdc1d36dbda6
gan <772435545@qq.com>
2017-08-31 16:32:01 +0800

    module文件冲突

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,9 @@ import {StarsComponent} from './components/stars/stars.component';
 import {PersonComponent} from './views/person/person.component';
 import {DetailsComponent} from './views/details/details.component';
 import {ContentService} from './service/content.service';
-<<<<<<< .merge_file_a11368
 import { CookbookComponent } from './views/cookbook/cookbook.component';
-=======
 import { SlideComponent } from './views/common/slide/slide.component';
 import { HomeComponent } from './views/home/home.component';
->>>>>>> .merge_file_a09128
 
 
 @NgModule({
@@ -25,12 +22,9 @@ import { HomeComponent } from './views/home/home.component';
         StarsComponent,
         PersonComponent,
         DetailsComponent,
-<<<<<<< .merge_file_a11368
-        CookbookComponent
-=======
+        CookbookComponent,
         SlideComponent,
         HomeComponent
->>>>>>> .merge_file_a09128
     ],
     imports: [
         BrowserModule,
