9301cc6786d575427ffad9d2883e7c14a4043951
liangjianmin <jianmin.liang@dolabank.com>
2017-09-04 11:22:45 +0800

    login resister

--- src/app/views/person/register/register.component.ts
+++ src/app/views/person/register/register.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+    constructor(public router: Router) {
 
-  ngOnInit() {
-    
-  }
+    }
+
+    ngOnInit() {
+
+    }
 
 }
